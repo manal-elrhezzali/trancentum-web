39a6a9fa1c71f9947b172c2739199836b5e212f1
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-06-05 20:14:42 +0100

    added a gridView builder widget to package status info card

--- lib/screens/home/components/package_status_info_card_grid_view.dart
+++ lib/screens/home/components/package_status_info_card_grid_view.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:trancentum_web_app/models/package_status_info.dart';
 
+import '../../../constants.dart';
+import 'header_package_info_card.dart';
+
+class PackageStatusInfoCardGridView extends StatelessWidget {
+  const PackageStatusInfoCardGridView({
+    Key key,
+    @required this.demoMyPackages,
+    this.crossAxisCount = 2,
+    this.childAspectRatio = 2.5,
+  }) : super(key: key);
+
+  final List<PackagesStatusInfo> demoMyPackages;
+
+  final int crossAxisCount;
+  final double childAspectRatio;
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: demoMyPackages.length, //dummy data
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        crossAxisSpacing: defaultPadding,
+        mainAxisSpacing: defaultPadding,
+        childAspectRatio: childAspectRatio,
+      ),
+      itemBuilder: (context, index) => HeaderPackageInfoCard(
+        info: demoMyPackages[index],
+        myPackages: demoMyPackages,
+      ),
+    );
+  }
+}

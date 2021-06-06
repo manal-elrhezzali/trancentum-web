ca7d705f1baf353bf7c5c5dbc4cac882bf576618
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-06-06 01:13:16 +0100

    wrapped row with center and signle childscrollView

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+
+import 'package:fl_chart/fl_chart.dart';
+
 import 'package:trancentum_web_app/constants.dart';
 import 'package:trancentum_web_app/models/expedition.dart';
 import 'package:trancentum_web_app/models/package_status_info.dart';
@@ -17,6 +20,50 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<PieChartSectionData> pieChartSectionData = [
+    PieChartSectionData(
+      //enregistree
+      value: 25,
+      color: primaryColor,
+      showTitle: false,
+      radius: 5,
+    ),
+    PieChartSectionData(
+      //chargee
+      value: 20,
+      color: kPrimaryColor,
+      showTitle: false,
+      radius: 5,
+    ),
+    PieChartSectionData(
+      //recue
+      value: 10,
+      color: Colors.green,
+      showTitle: false,
+      radius: 5,
+    ),
+    PieChartSectionData(
+      //livree
+      value: 20,
+      color: Colors.brown,
+      showTitle: false,
+      radius: 5,
+    ),
+    PieChartSectionData(
+      //retour
+      value: 15,
+      color: Colors.red,
+      showTitle: false,
+      radius: 5,
+    ),
+    PieChartSectionData(
+      //cloturee
+      value: 18,
+      color: Colors.orange,
+      showTitle: false,
+      radius: 5,
+    ),
+  ];
   final List<PackagesStatusInfo> demoMyPackages = [
     PackagesStatusInfo(
       title: "Enregistrée",
@@ -49,106 +96,201 @@ class _BodyState extends State<Body> {
       color: Colors.orange,
     ),
   ];
-  bool showAll = false;
+  bool showAllRecentPackages = false;
+  bool showAllNotifications = false;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(defaultPadding * 2),
-      child: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //status
-                Container(
-                  // padding: EdgeInsets.all(defaultPadding),
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: darkBgColor),
-                  child: Text(
-                    "Status Expeditions",
-                    style: TextStyle(
-                      color: whiteColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                SizedBox(height: defaultPadding),
-                //package status header
-                PackageStatusInfoCardGridView(
-                  demoMyPackages: demoMyPackages,
-                  tapHandler: () {},
-                ),
-                SizedBox(height: defaultPadding),
-                Container(
-                  // padding: EdgeInsets.all(defaultPadding),
-                  width: double.infinity,
-                  decoration: BoxDecoration(color: darkBgColor),
-                  child: Text(
-                    "Expeditions Consultées",
-                    style: TextStyle(
-                      color: whiteColor,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                  ),
-                ),
-                SizedBox(height: defaultPadding),
-                //expedition consultees
-                RecentExpeditionCardGridView(
-                  childAspectRatio: 5,
-                  tapHandler: () {},
-                  crossAxisCount: 1,
-                  demoRecentExpeditions: demoRecentExpeditions,
-                  showAll: showAll,
-                ),
-                SizedBox(height: defaultPadding),
-                Container(
-                  padding: EdgeInsets.all(defaultPadding / 2),
-                  width: double.infinity,
-                  color: bgColor,
-                  child: FlatButton(
-                    onPressed: () {
-                      setState(() {
-                        showAll = !showAll;
-                      });
-                    },
+    return Center(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.all(defaultPadding * 2),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  //status
+                  Container(
+                    // padding: EdgeInsets.all(defaultPadding),
+                    width: double.infinity,
+                    decoration: BoxDecoration(color: darkBgColor),
                     child: Text(
-                      showAll ? "REDUIRE" : "TOUT AFFICHER",
+                      "Status Expeditions",
                       style: TextStyle(
                         color: whiteColor,
                         fontWeight: FontWeight.bold,
-                        fontSize: 15,
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(height: defaultPadding),
+                  //package status header
+                  PackageStatusInfoCardGridView(
+                    demoMyPackages: demoMyPackages,
+                    tapHandler: () {},
+                  ),
+                  SizedBox(height: defaultPadding),
+                  Container(
+                    // padding: EdgeInsets.all(defaultPadding),
+                    width: double.infinity,
+                    decoration: BoxDecoration(color: darkBgColor),
+                    child: Text(
+                      "Expeditions Consultées",
+                      style: TextStyle(
+                        color: whiteColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: defaultPadding),
+                  //expedition consultees
+                  RecentExpeditionCardGridView(
+                    childAspectRatio: 5,
+                    tapHandler: () {},
+                    crossAxisCount: 1,
+                    demoRecentExpeditions: demoRecentExpeditions,
+                    showAll: showAllRecentPackages,
+                  ),
+                  SizedBox(height: defaultPadding),
+                  Container(
+                    padding: EdgeInsets.all(defaultPadding / 2),
+                    width: double.infinity,
+                    color: bgColor,
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          showAllRecentPackages = !showAllRecentPackages;
+                        });
+                      },
+                      child: Text(
+                        showAllRecentPackages ? "REDUIRE" : "TOUT AFFICHER",
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(width: defaultPadding),
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                Container(
-                  width: double.infinity,
-                  color: whiteColor,
-                  child: Text("Chart Container hereeeee"),
-                ),
-                SizedBox(height: defaultPadding * 2),
-                Container(
-                  width: double.infinity,
-                  color: redColor,
-                  child: Text("Notifications Container hereeeee"),
-                ),
-              ],
+            SizedBox(width: defaultPadding),
+            Expanded(
+              child: Column(
+                // crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(color: darkBgColor),
+                    child: Text(
+                      "Graphe Expeditions",
+                      style: TextStyle(
+                        color: whiteColor,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: defaultPadding),
+                  Container(
+                    width: double.infinity,
+                    height: 200,
+                    child: PieChart(
+                      PieChartData(
+                        sectionsSpace: 0,
+                        centerSpaceRadius: 100,
+                        startDegreeOffset: -90,
+                        sections: pieChartSectionData,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: defaultPadding * 2),
+                  //Notifications
+                  Container(
+                    width: double.infinity,
+                    // color: ,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Notifications",
+                          style: TextStyle(
+                            color: whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
+                        SizedBox(height: defaultPadding),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: showAllNotifications ? 6 : 3,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              color: bgColor,
+                              elevation: 0,
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(defaultPadding),
+                                leading: Icon(
+                                  Icons.notification_important_outlined,
+                                  color: kSecondaryColor,
+                                ),
+                                title: Text(
+                                  "I am a notification notification notification notification",
+                                  maxLines: 2,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: whiteColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                trailing: IconButton(
+                                  onPressed: () {
+                                    //delete the notification
+                                  },
+                                  icon: Icon(
+                                    Icons.delete_forever_outlined,
+                                    color: redColor,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        SizedBox(height: defaultPadding),
+                        Container(
+                          padding: EdgeInsets.all(defaultPadding / 2),
+                          width: double.infinity,
+                          color: bgColor,
+                          child: FlatButton(
+                            onPressed: () {
+                              setState(() {
+                                showAllNotifications = !showAllNotifications;
+                              });
+                            },
+                            child: Text(
+                              showAllNotifications
+                                  ? "REDUIRE"
+                                  : "TOUT AFFICHER",
+                              style: TextStyle(
+                                color: whiteColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

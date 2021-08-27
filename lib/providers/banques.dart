3e840a3bea461a93885b6419ac10bc99b1981aed
El-rhezzali Manal <manal.gzl99@gmail.com>
2021-08-27 22:18:54 +0100

    deleted chart dummy data

--- lib/providers/banques.dart
+++ lib/providers/banques.dart
@@ -5,7 +5,6 @@ class Banques with ChangeNotifier {
   List<Banque> _items = [
     Banque(id: "B1", nom: "Chaabi", code: "CHA1234"),
     Banque(id: "B2", nom: "BMCE", code: "BM98645"),
-
   ];
   List<Banque> get items {
     return [..._items];
@@ -14,10 +13,9 @@ class Banques with ChangeNotifier {
   int get itemCount {
     return _items.length;
   }
+
   Banque findById(String banqueId) {
-    return _items.firstWhere(
-        (banque) => banque.id == banqueId,
+    return _items.firstWhere((banque) => banque.id == banqueId,
         orElse: () => null);
   }
-
 }
